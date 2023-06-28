4d061b59e91a0f85d4ca188529e009c6afcb616c
FelipeSva <felipe.silva.pt@outlook.pt>
2023-06-28 01:07:25 -0300

    Create InferenciaTipo.java
    Aqui aprendo que, em alguns casos, uma variavel não foi declarada pelo seu tipo, mas sim pelo seu valor recebido. Por exemplo, se eu usar o "var", dar um nome a variavel e colocar o valor que ela recebe, esse valor determinara se sera uma String, inteiro, double e etc.

---
+++ src/InferenciaTipo.java
@@ -0,0 +1,12 @@
+
+public class InferenciaTipo {
+	public static void main(String[] args) {
+		
+		double a = 4.5; //determinei um tipo e um valor.
+		System.out.println(a);
+		
+		var b = 4.5; 
+		System.out.println(b);
+		
+	}
+}
