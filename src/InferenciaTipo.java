091a02e846c80e6aa43c725234e0550fe595b99e
FelipeSva <felipe.silva.pt@outlook.pt>
2023-06-28 01:11:38 -0300

    Update InferenciaTipo.java

--- src/InferenciaTipo.java
+++ src/InferenciaTipo.java
@@ -5,8 +5,16 @@ public class InferenciaTipo {
 		double a = 4.5; //determinei um tipo e um valor.
 		System.out.println(a);
 		
-		var b = 4.5; 
+		var b = 4.5; // não determinei o tipo, mas usando o var,
+		//pelo valor dado, se é atribuido o tipo da variavel.
 		System.out.println(b);
+		
+		var c = "texto"
+		System.out.println(c);
+		
+		double d; // variavel foi declarada
+		d = 123.65; // variavel foi inicializada
+		System.out.println(d); // usada! 
 		
 	}
 }
