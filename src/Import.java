a27209c7949582da1b64c54757a1476565a5ece9
FelipeSva <felipe.silva.pt@outlook.pt>
2023-06-28 22:33:48 -0300

    Import
    Nem tudo o java me entregara de mão beijada. Por isso, quando eu precisar que as classes falem entre si, terei que usar import para tal, ou ate mesmo funções que o pacote que estou usando nao possui e por isso devo importar para o mesmo.

---
+++ src/Import.java
@@ -0,0 +1,21 @@
+import java.util.Date;
+
+import javax.swing.JButton;
+
+public class Import {
+	public static void main(String[] args) {
+		
+		
+		String s = "Bom dia meu amigo";
+		System.out.println(s);
+		
+		Date data = new Date();
+		System.out.println(data);
+	    
+		JButton botao = new JButton();
+		//System.out.println(botao);
+		
+		
+		}
+
+}
