520d304bd30ab68c5b17864d2d7ccd9ff0c0d6b3
FelipeSva <felipe.silva.pt@outlook.pt>
2023-07-02 17:49:13 -0300

    Converter Inteiro em String
    De forma simples, eu consigo fazer essa conversao usando um Wrapper, a classe orientada a objeto do tipo inteiro que é INTEGER.
    
    Para que eu possa fazer a mesma conversao usando o tipo primitivo INT, eu preciso imprimir o INTEGER.TOSTRING e colocar o nome da variavel inteira que fiz anteriormente. De forma simples, terei agora uma String.

---
+++ src/ConversaoNumeroString.java
@@ -0,0 +1,11 @@
+
+public class ConversaoNumeroString {
+	public static void main(String[] args) {
+		
+		Integer num1 = 1000;
+		System.out.println(num1.toString().length());
+		
+		int num2 = 2000;
+		System.out.println(Integer.toString(num2));
+	}
+}
